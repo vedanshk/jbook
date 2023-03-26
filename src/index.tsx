db7a3db4c7a3fd95454d36c3d68a06d336473be2
vedansh kumar <56169665+vedanshk@users.noreply.github.com>
2023-03-26 21:22:06 +0530

    added textview and esbuild for previewing code

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as esbuild from 'esbuild-wasm';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { useEffect, useState , useRef } from 'react';
+import {createRoot} from 'react-dom/client';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const el = document.getElementById('root');
+
+const root = createRoot(el!);
+
+const App = ()  =>{
+    const [input , setInput] = useState('');
+    const [code , setCode] =  useState('');
+    const ref = useRef<any>();
+
+    const startService = async () =>{
+
+      ref.current =  await esbuild.startService({
+            worker:true, 
+            wasmURL:"/esbuild.wasm"
+        });
+        
+
+    };
+
+    const onChange = (e:React.ChangeEvent<HTMLTextAreaElement>) =>{
+        setInput(e.target.value);
+    }
+    const onClick  =  async  () =>{
+
+        if(!ref.current){
+                return ;
+        }
+
+
+        const result = await ref.current.transform(input , {
+            loader:'jsx',
+            target: 'es2015'
+        })
+
+        
+        setCode(result.code);
+
+
+    } 
+
+    useEffect(()=>{
+
+        startService();
+
+    } , []);
+
+    return <div>
+        <textarea onChange={onChange} value={input}/>
+        <div>
+            <button onClick={onClick}>Submit</button>
+        </div>
+        <pre>{code}</pre>
+    </div>
+}
+
+root.render(<App />);
+
+
+
